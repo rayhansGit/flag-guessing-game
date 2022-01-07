a76832db14f2f689c414c27610b850dd13d80f2f
Md. Rayhan Al Islam <showrov.rayhan@gmail.com>
2022-01-07 16:49:07 +0200

    Update flag_game.py

--- flag_game.py
+++ flag_game.py
@@ -1,17 +1,3 @@
-"""
-COMP.CS.100 Ohjelmointi 1 / Programming 1
-Student Id: 150161349
-Name:       Md Rayhan Al Islam
-Email:      md.islam@tuni.fi
-
-Project for programming 1 course:
-I have designed a flag guessing game using the tkinter ui library. The game is simple and fun to play
-First step is providing the number of rounds you want to play. After that, the flag images will start poping and
-you have to guess the name of that country.
-After finishing the round, the result window will pop up and show you the total right guesses you made. You are allowed
-to write the country name in caps or lower case.
-The system is designed in such a way that it will show random flags everytime you run this
-"""
 import os
 import random
 from tkinter import *
